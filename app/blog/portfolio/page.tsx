b2d86b6550b53bc4c6b526f949f5627231ae41cc
dmajor007 <diallo.dixon@gmail.com>
2026-08-21 15:18:13 -0500

    changed heading of portfolio page

--- app/blog/portfolio/page.tsx
+++ app/blog/portfolio/page.tsx
@@ -3,5 +3,5 @@ import ArticleList from '@/components/ArticleList';
 export const revalidate = 60;
 
 export default function PortfolioPage() {
-  return <ArticleList type="portfolio" title="AI Content Showcase" />;
+  return <ArticleList type="portfolio" title="Blog" />;
 }
